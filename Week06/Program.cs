a89552b304d92fe52b0603c10ae333c0c2ca465a
eventpb <59030195@kmitl.ac.th>
2017-09-15 10:55:39 +0700

    W06P11

--- Week06/Program.cs
+++ Week06/Program.cs
@@ -3,27 +3,26 @@
 class Program
 {
     static void Main()
-    {
+    { 
+    
         int x = 17;
-        double y = x / 4.0;
-        double z = y * y;
-        Console.WriteLine(y);
+    double y = x / 4.0;
+    double z = y * y;
+    Console.WriteLine(y);
         Console.WriteLine(z);
-    }
+    
 
-    static void Helloworld()
-    {
-        int a = 5;
-        int b = 4;
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (b++));
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (++b));
-        Console.WriteLine(a + b);
-        Console.WriteLine(14 / a);
-        Console.WriteLine(14 % a);
-        Console.ReadLine();
-    }
+   
+         int a = 5;
+    int b = 4;
+    Console.WriteLine(a + b);
+         Console.WriteLine(a + (b++));
+         Console.WriteLine(a + b);
+         Console.WriteLine(a + (++b));
+         Console.WriteLine(a + b);
+         Console.WriteLine(14 / a);
+         Console.WriteLine(14 % a);
+        }
 }
 
 
